edac09ed4ac60285dfdf84ed89f210cb7bfb9762
riebow <asrm8d@mail.umsl.edu>
2020-02-26 20:08:14 -0600

    Added comments to code, added README, fixed a few more things

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+//Andrew Riebow
+//4760, Project 2
+//February 26, 2020
+
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
@@ -48,6 +52,7 @@ int main(int argc, char *argv[]){
 	long tmp;
 	char *eptr;
 
+	//Get options, just like in the first project
 	while ((option = getopt(argc, argv, "hn:s:b:i:o:")) != -1){
 		switch(option){
 			case 'h':
@@ -65,8 +70,8 @@ int main(int argc, char *argv[]){
 				break;
 			case 'n':
 				nFlag = 1;
-				errno = 0;
-				tmp = strtol(optarg, eptr, 10);
+				errno = 0; //Reset errno to check if an error occurs right after
+				tmp = strtol(optarg, eptr, 10); //To preserve our default, put the value of strtol in a temporary variable in case it errors
 				if(errno != 0 || tmp == 0){
 					char *errorMessage[150];
 					strcat(errorMessage, programName);
@@ -76,7 +81,6 @@ int main(int argc, char *argv[]){
 				else{
 					nValue = abs(tmp);
 				}
-				printf("nValue: %d\n", nValue);
 				break;
 			case 's':
 				sFlag = 1;
@@ -92,7 +96,6 @@ int main(int argc, char *argv[]){
 					sValue = abs(tmp);
 					if(sValue > 20) sValue = 20;
 				}
-				printf("sValue: %d\n", sValue);
 				break;
 			case 'b':
 				bFlag = 1;
@@ -107,7 +110,6 @@ int main(int argc, char *argv[]){
 				else{
 					bValue = abs(tmp);
 				}
-				printf("bValue: %d\n", bValue);
 				break;
 			case 'i':
 				iFlag = 1;
@@ -122,7 +124,6 @@ int main(int argc, char *argv[]){
 				else{
 					iValue = abs(tmp);
 				}
-				printf("iValue: %d\n", iValue);
 				break;
 			case 'o':
 				if(optarg != NULL){
@@ -131,24 +132,20 @@ int main(int argc, char *argv[]){
 				}
 				break;
 		}
-		//printf("%s\n", optarg);
+		
 	}
-	printf("oValue: %s\n", oValue);
-	printf("ovalue second place: %c\n", oValue[1]);
 
-	clock_t before = clock();
-
-
-	key_t key;
-	key = ftok(".", 897);
-	shmid = shmget(key, sizeof(int) * nValue, 0666 | IPC_CREAT);
+	//Create shared memory
+	key_t key; //Create a key for the shared memory
+	key = ftok(".", 897); //Create a unique key
+	shmid = shmget(key, sizeof(int) * nValue, 0666 | IPC_CREAT); //Allocate memory
 	if(shmid < 0){
 		char *errorMessage[150];
 		strcat(errorMessage, programName);
 		strcat(errorMessage, ": Error: Failed to create shared memory");
 		perror(errorMessage);
 	}
-	shmPointer = shmat(shmid, NULL, 0);
+	shmPointer = shmat(shmid, NULL, 0); //Attach the memory
 	if (shmPointer == (void *) -1){
 		char *errorMessage[150];
 		strcat(errorMessage, programName);
@@ -156,7 +153,13 @@ int main(int argc, char *argv[]){
 		perror(errorMessage);
 	}
 	
+	//Initialize shared mem locations to 0
+	int j;
+	for(j = 2; j < nValue + 2; j++){
+		shmPointer[j] = 0;
+	}
 
+	//Set up our clock
 	struct timespec time;
 	clock_gettime(CLOCK_MONOTONIC, &time);
 
@@ -165,47 +168,65 @@ int main(int argc, char *argv[]){
 	int afterTimeSec = time.tv_sec - startingTimeSec;
 	long afterTimeMSec = time.tv_nsec / 1000000 - startingTimeMSec;
 
-	shmPointer[0] = afterTimeSec;
-	shmPointer[1] = afterTimeMSec;
+	shmPointer[0] = afterTimeSec; //Set the seconds to the first array location
+	shmPointer[1] = afterTimeMSec; //Set the miliseconds to the second array location
 
 
-	char bString[10];
+	char bString[10]; //Create variables for b and id to be passed as paramters because they must be strings
 	char idString[10];
-	int numOfChildren = 0;
+	int numOfChildren = 0; //Create a counter for number of children currently running
 
+	//Main loop for creating child processes
 	int i;
 	for(i = 0; i < nValue; i ++){	
 		pid_t childPid;
-		if(numOfChildren >= sValue){
-			wait(NULL);
-			numOfChildren--;
+		if(numOfChildren >= sValue){ //If we have max number of children specified from the options
+			wait(NULL); //Wait until any child exits
+			numOfChildren--;//Decrement number of child processes counter
 		}
-		updateTime(time, shmPointer, startingTimeSec, startingTimeMSec);
+		updateTime(time, shmPointer, startingTimeSec, startingTimeMSec); //Function to update time in shared memory, right before we create a new process so they can get their start time
 		 
-		childPid = fork();
+		childPid = fork(); //Create child process
 		
-
+		//reset string variables to empty
 		bString[0] = '\0';
 		idString[0] = '\0';
 		sprintf(idString, "%d", i + 2);
 		sprintf(bString, "%d", bValue);
+
+		//Child process
 		if(childPid == 0){
-		printf("This is the child process\n");
+
 		char *paramList[] = {"./prime", idString, bString, oValue, NULL};
 		execv("prime", paramList);
 		exit(0);
 		}
-		else if(childPid < 0){
+		else if(childPid < 0){ //If fork failed
 			char *errorMessage[150];
 			strcat(errorMessage, programName);
 			strcat(errorMessage, ": Error: fork() failed to create child");
 			perror(errorMessage);
 		}
 		else{
-			numOfChildren++;
+			numOfChildren++; //Parent process
 		}
-		bValue = bValue + iValue;
-		if(shmPointer[0] >= 2 && (shmPointer[1] - startingTimeMSec) > 0){
+		bValue = bValue + iValue; //Increase the next number to be given to process by the increment option value
+
+		if(shmPointer[0] >= 2 && (shmPointer[1] - startingTimeMSec) > 0){ //If it has been more than 2 seconds in time, kill children, exit parent process, and clear memory
+			printf("Parent execution time exceeded 2 seconds, exiting...\n");
+			shmdt(shmPointer);
+			shmctl(shmid, IPC_RMID, 0);
+			kill(0, SIGKILL);
+			exit(-1);
+		}
+	}
+
+	//Wait in a loop for children to finish, check max time again
+	pid_t allChildrenPid;
+	int status = 0;
+	while ((allChildrenPid = wait(&status)) > 0){
+		numOfChildren--;
+		if(shmPointer[0] >= 2 && (shmPointer[1] - startingTimeMSec) > 0){ //If it has been more than 2 seconds in time, kill children, exit parent process, and clear memory
 			printf("Parent execution time exceeded 2 seconds, exiting...");
 			shmdt(shmPointer);
 			shmctl(shmid, IPC_RMID, 0);
@@ -213,22 +234,17 @@ int main(int argc, char *argv[]){
 			exit(-1);
 		}
 	}
-	printf("Parent to wait for children...\n");
-
-	pid_t allChildrenPid;
-	int status = 0;
-	while ((allChildrenPid = wait(&status)) > 0){
-		numOfChildren--;
-	}
 	
 
 	updateTime(time, shmPointer, startingTimeSec, startingTimeMSec);
 	
+	//Output numbers to log file
 	char output[200];
-	int file = open(oValue, O_CREAT | O_RDWR | O_APPEND, 0666);	
+	int file = open(oValue, O_CREAT | O_RDWR | O_APPEND, 0666); //Open file
 	sprintf(output, "\nPrime Numbers: ");
 	write(file, output, strlen(output));
 	
+	//Iterate over the shared memory array, and depending on what the number is, output it in a specific section
 	for(i = 2; i < nValue + 2; i++){
 		output[0] = '\0';
 		if(shmPointer[i] >= 0){
@@ -253,10 +269,11 @@ int main(int argc, char *argv[]){
 		}
 	}
 	sprintf(output, "%d", failedCount);
-	strcat(output, "\n");
+	strcat(output, "\n\n");
 	write(file, output, strlen(output));
 	close(file);
 
+	//Detach and delete shared memory
 	if((shmdt(shmPointer)) < 0){
 		char *errorMessage[150];
 		strcat(errorMessage, programName);
@@ -269,11 +286,11 @@ int main(int argc, char *argv[]){
 		strcat(errorMessage, ": Error: Failed to delete shared memory");
 		perror(errorMessage);	
 	}
+	printf("Parent finished\n");
 	return 0;
 }
 
-
-
+//Function for handling ctrl-c signal
 void signalHandler(int signal){
 	printf("Canceled with ctrl-c, stopping children...\n");
 	kill(0, SIGINT);
@@ -284,6 +301,7 @@ void signalHandler(int signal){
 	
 }
 
+//Function to update the clock in shared memory with the current time. Called periodically throughout program
 void updateTime(struct timespec time, int *shmPointer, int startingSec, long startingMSec){
 	clock_gettime(CLOCK_MONOTONIC, &time);
 	shmPointer[0] = time.tv_sec - startingSec;
--- prime.c
+++ prime.c
@@ -1,3 +1,7 @@
+//Andrew Riebow
+//4760, Project 2
+//February 26, 2020
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/types.h>
@@ -9,10 +13,12 @@
 #include<fcntl.h>
 #include<signal.h>
 
+//Define function prototype
 void killSigHandler();
 
+//Declare globals
 int file;
-char output[50];
+char output[90];
 char outputFilename[50];
 
 int timeStartSec;
@@ -24,11 +30,13 @@ int *shmPointer;
 
 int main(int argc, char *argv[]){	
 	
+	//Change signal handling
 	signal(SIGINT, killSigHandler);
 
 	
-	printf("This is execed child\n");
+	//Put output filename from arguments into varible
 	strcat(outputFilename, argv[3]);
+	//Get shared memory and attach
 	key_t key;
 	key = ftok(".", 897);
 	int shmid = shmget(key, 0, 0666);
@@ -41,6 +49,7 @@ int main(int argc, char *argv[]){
 		perror("Exec could not attach shared memory");
 	}
 
+	//Open a file to output when this child started
 	file = open(argv[3], O_CREAT | O_RDWR | O_APPEND, 0666);	
 	sprintf(output, "Child ID: %s started at %d seconds, %d miliseconds\n", argv[1], shmPointer[0], shmPointer[1]);
 	write(file, output, strlen(output));
@@ -52,7 +61,7 @@ int main(int argc, char *argv[]){
 
 	errno = 0;
 	id = strtol(argv[1], NULL, 10);
-	int numToCheck = strtol(argv[2], NULL, 10);
+	int numToCheck = strtol(argv[2], NULL, 10);//Get number to check from arguments
 
 	if(numToCheck == 0){
 		perror("could not strtol argv[1]");
@@ -66,20 +75,18 @@ int main(int argc, char *argv[]){
 		perror("Child Process failed to find prime number");
 		exit(-1);
 	}
-	printf("strtol argv[1], %d\n", id);
-	printf("strtol argv[2], %d\n", numToCheck);
 	
 	//Calculate Prime
 	int isPrime = 1;
 	int i;
 	shmPointer[id] = 5;
-	printf("Checking\n");
 	for(i=1;i<numToCheck;i++){
-		if(numToCheck % i == 0 && i != 1 && i != numToCheck){	
+		if(numToCheck % i == 0 && i != 1 && i != numToCheck){	//If the number is divisible by only 1 and itself
 			isPrime = 0;
 			shmPointer[id] = numToCheck * -1;
 		}
-		if(abs(shmPointer[1] - timeStartMSec) > 100){
+		if(abs(shmPointer[1] - timeStartMSec) > 100){ //if more than 100 ms has passed, exit
+			printf("Child ID: %d has run for more than 100ms, exiting...\n", id);
 			isPrime = 0;
 			shmPointer[id] = -1;
 			break;
@@ -90,26 +97,28 @@ int main(int argc, char *argv[]){
 	}
 
 
-	printf("isPrime = %d\n", isPrime);
-	
+	//Create output text for file based on whether the number was prime
 	if(isPrime == 1 && shmPointer[id] != -1){
-		sprintf(output, "Child ID: %d found %d to be prime at %d seconds, %d mili seconds\n", id, numToCheck, shmPointer[0], shmPointer[1]);
+		sprintf(output, "Child ID: %d found %d to be prime at %d seconds, %d miliseconds\n", id, numToCheck, shmPointer[0], shmPointer[1]);
 	}
-	else if(isPrime == 0){
-		sprintf(output, "Child ID: %d found %d to NOT be prime at %d seconds, %d mili seconds\n", id, numToCheck, shmPointer[0], shmPointer[1]);	
+	else if(isPrime == 0 && shmPointer[id] != -1){
+		sprintf(output, "Child ID: %d found %d to NOT be prime at %d seconds, %d miliseconds\n", id, numToCheck, shmPointer[0], shmPointer[1]);	
 	}
 	else{
-		sprintf(output, "Child ID: %d exited early at %d seconds, %d mili seconds\n", id, shmPointer[0], shmPointer[1]);
+		sprintf(output, "Child ID: %d exited early at %d seconds, %d miliseconds\n", id, shmPointer[0], shmPointer[1]);
 	}
 
-	shmdt(shmPointer);
+	shmdt(shmPointer);//Detach memory
 	
+	//Output to file, create if it does not exist
 	file = open(argv[3], O_CREAT | O_RDWR | O_APPEND, 0666);	
 	write(file, output, strlen(output));
 	close(file);
+	printf("Child %d finished\n", id);
 	exit(0);
 }
 
+//Function for handling signal
 void killSigHandler(){
 	sprintf(output, "Child ID: %d exited early at %d seconds, %d mili seconds\n", id, shmPointer[0], shmPointer[1]);
 	file = open(outputFilename, O_CREAT | O_RDWR | O_APPEND, 0666);	
